19a759558e3bfd0e07b607c3756ceb936471db23
Nanichang Katzing <nanipaul68@gmail.com>
2020-04-03 23:50:27 +0100

    added toastr

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,14 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+import toastr from 'toastr/toastr';
+
 import "bootstrap"
 import "../stylesheets/application"
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require toastr
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
